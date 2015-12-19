91bdb4511d453ccbf934aaec54249f7660c821f4
Elaine Thomas <enajera2@cnm.edu>
2015-12-19 07:46:15 -0700

    Added practice php file that utilizes a for loop

---
+++ phptest.php
@@ -0,0 +1,7 @@
+<?php
+for($catFoodCans = 5; $catFoodCans > -1; --$catFoodCans)
+{
+	if ($catFoodCans >= 1)
+		{echo "There are $catFoodCans  cans of cat food left.  <br>";};
+	if ($catFoodCans == 0)
+		{echo "Time to go to the store!";};}
